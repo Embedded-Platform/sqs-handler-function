b52aed490264526b33691731fcaeec7048e3ac9c
Cristian David Ortiz Henao <Cristian.Ortiz@ibm.com>
2024-04-16 21:43:44 -0500

    Update typescript project

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 console.log('Loading function');
+import {Handler, SQSEvent} from 'aws-lambda';
 
-export const handler = async (event) => {
+export const handler: Handler = async (event: SQSEvent): Promise<string> => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     for (const { messageId, body } of event.Records) {
         console.log('SQS message %s: %j', messageId, body);
